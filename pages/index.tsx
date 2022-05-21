2fdd645c75b1e0b43ab3c7b67b5383ef37dbab35
Scott Martin <scott.martin88@live.com>
2022-05-21 21:27:00 +0100

    Comment functionality implement

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,6 @@ interface Props {
 }
 
 const Home = ({ tweets }: Props) => {
-  console.log(tweets)
-
   return (
     <div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
       <Head>
@@ -22,7 +20,7 @@ const Home = ({ tweets }: Props) => {
 
       <main className="grid grid-cols-9">
         <Sidebar />
-        <Feed />
+        <Feed tweets={tweets} />
         <Widgets />
       </main>
     </div>
