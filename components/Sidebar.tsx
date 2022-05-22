84d7d5bf81e219f04d77e49c2bc1cbcd9eedcc15
Scott Martin <scott.martin88@live.com>
2022-05-22 07:20:57 +0100

    React hot toast notifications and twitter login func with NextAuth implement

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,9 +8,12 @@ import {
   UserIcon,
   HomeIcon,
 } from '@heroicons/react/outline'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import SidebarRow from './SidebarRow'
 
 function Sidebar() {
+  const { data: session } = useSession()
+
   return (
     <div className="col-span-2 flex flex-col items-center px-4 md:items-start">
       <img
@@ -24,7 +27,11 @@ function Sidebar() {
       <SidebarRow Icon={MailIcon} title="Messages" />
       <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
       <SidebarRow Icon={CollectionIcon} title="Lists" />
-      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow
+        onClick={session ? signOut : signIn}
+        Icon={UserIcon}
+        title={session ? 'Sign Out' : 'Sign In'}
+      />
       <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
     </div>
   )
